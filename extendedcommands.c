CyanogenMod/android_bootable_recovery

Make updating Advanced Menu a simpler task
Change-Id: Iaf86e006ff4a67e72fe6c8141068ec2e36360671

extendedcommands.c
@@ -91,7 +91,7 @@ void write_string_to_file(const char* filename, const char* string) {
 
 void write_recovery_version() {
     char path[PATH_MAX];
-    sprintf(path, "%s%sclockworkmod/.recovery_version", get_primary_storage_path(), (is_data_media() ? "/0/" : "/"));
+    sprintf(path, "%s/clockworkmod/.recovery_version", get_primary_storage_path());
     write_string_to_file(path,EXPAND(RECOVERY_VERSION) "\n" EXPAND(TARGET_DEVICE));
 }
 
@@ -1237,12 +1237,12 @@ int can_partition(const char* volume) {
     return 1;
 }
 
-
+#define FIXED_ADVANCED_MENU_ENTRIES 7
 int show_advanced_menu()
 {
     char buf[80];
     int i = 0, j = 0, chosen_item = 0;
-    static char* list[MAX_NUM_MANAGED_VOLUMES + 9];
+    static char* list[MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_MENU_ENTRIES + 1];
 
     char* primary_path = get_primary_storage_path();
     char** extra_paths = get_extra_storage_paths();
@@ -1253,7 +1253,7 @@ int show_advanced_menu()
                                 NULL
     };
 
-    memset(list, 0, MAX_NUM_MANAGED_VOLUMES + 9);
+    memset(list, 0, MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_MENU_ENTRIES + 1);
 
     list[0] = "reboot recovery";
 
@@ -1271,21 +1271,23 @@ int show_advanced_menu()
     list[5] = "key test";
     list[6] = "show log";
 
+    char list_prefix[] = "partition ";
     if (can_partition(primary_path)) {
-        sprintf(buf, "partition %s", primary_path);
-        list[7] = strdup(buf);
+        sprintf(buf, "%s%s", list_prefix, primary_path);
+        list[FIXED_ADVANCED_MENU_ENTRIES] = strdup(buf);
         j++;
     }
 
     if (extra_paths != NULL) {
         for (i = 0; i < num_extra_volumes; i++) {
             if (can_partition(extra_paths[i])) {
-                sprintf(buf, "partition %s", extra_paths[i]);
-                list[7 + j] = strdup(buf);
+                sprintf(buf, "%s%s", list_prefix, extra_paths[i]);
+                list[FIXED_ADVANCED_MENU_ENTRIES + j] = strdup(buf);
                 j++;
             }
         }
     }
+    list[FIXED_ADVANCED_MENU_ENTRIES + j] = NULL;
 
     for (;;)
     {
@@ -1351,20 +1353,14 @@ int show_advanced_menu()
             case 6:
                 ui_printlogtail(12);
                 break;
-            case 7:
-                partition_sdcard(primary_path);
-                break;
             default:
-                if (chosen_item >= 8) {
-                    partition_sdcard(list[chosen_item] + 10);
-                }
+                partition_sdcard(list[chosen_item] + strlen(list_prefix));
                 break;
         }
     }
-    free(list[7]);
-    if (extra_paths != NULL) {
-        for (; j >= 0; --j)
-            free(list[8 + j]);
+
+    for(; j > 0; --j) {
+        free(list[FIXED_ADVANCED_MENU_ENTRIES + j - 1]);
     }
     return chosen_item;
 }

garwedgess/LuPuS-CWM

extendedcommands.c
@@ -1357,7 +1357,7 @@ int show_advanced_menu()
     char buf[80];
     int i = 0, j = 0, chosen_item = 0;
     /* Default number of entries if no compile-time extras are added */
-    static char* list[MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_ENTRIES + 2];
+    static char* list[MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_ENTRIES + 1];
 
     char* primary_path = get_primary_storage_path();
     char** extra_paths = get_extra_storage_paths();
@@ -1368,7 +1368,7 @@ int show_advanced_menu()
                                 NULL
     };
 
-    memset(list, 0, MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_ENTRIES + 2);
+    memset(list, 0, MAX_NUM_MANAGED_VOLUMES + FIXED_ADVANCED_ENTRIES + 1);
 
     list[0] = "reboot recovery";
 
@@ -1389,8 +1389,9 @@ int show_advanced_menu()
     list[7] = "toggle loki support";
 #endif
 
+    char list_prefix[] = "partition ";
     if (can_partition(primary_path)) {
-        sprintf(buf, "partition %s", primary_path);
+        sprintf(buf, "%s%s", list_prefix, primary_path);
         list[FIXED_ADVANCED_ENTRIES] = strdup(buf);
         j++;
     }
@@ -1398,12 +1399,13 @@ int show_advanced_menu()
     if (extra_paths != NULL) {
         for (i = 0; i < num_extra_volumes; i++) {
             if (can_partition(extra_paths[i])) {
-                sprintf(buf, "partition %s", extra_paths[i]);
+                sprintf(buf, "%s%s", list_prefix, extra_paths[i]);
                 list[FIXED_ADVANCED_ENTRIES + j] = strdup(buf);
                 j++;
             }
         }
     }
+    list[FIXED_ADVANCED_ENTRIES + j] = NULL;
 
     for (;;)
     {
@@ -1474,20 +1476,14 @@ int show_advanced_menu()
                 toggle_loki_support();
                 break;
 #endif
-            case FIXED_ADVANCED_ENTRIES:
-                partition_sdcard(primary_path);
-                break;
             default:
-                if (chosen_item >= (FIXED_ADVANCED_ENTRIES+1)) {
-                    partition_sdcard(list[chosen_item] + 10);
-                }
+                partition_sdcard(list[chosen_item] + strlen(list_prefix));
                 break;
         }
     }
-    free(list[FIXED_ADVANCED_ENTRIES]);
-    if (extra_paths != NULL) {
-        for (; j >= 0; --j)
-            free(list[FIXED_ADVANCED_ENTRIES + 1 + j]);
+
+    for (; j > 0; --j) {
+        free(list[FIXED_ADVANCED_ENTRIES + j - 1]);
     }
     return chosen_item;
 }